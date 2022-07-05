330f0b954f7588560b23ac6869db40019909e44a
Thiago Lutten Leitão <thiago.leitao@direitosbc.br>
2022-07-05 16:44:41 -0300

    Subindo mudanças da aula de 01/07/2022

--- converter.py
+++ converter.py
@@ -2,19 +2,29 @@ import subprocess
 from pytube import YouTube
 import os
 import this
+import ffmpeg
+
 
 class Converter:
 
     def __init__(self, video_url):
         this.__video_url = video_url
+        this.__caminho = os.getcwd()  # Pegar o caminho da pasta em uso e adicionar a pasta arquivos_mp3
         this.__caminho = os.getcwd() + '\\arquivos_mp3'  # Pegar o caminho da pasta em uso e adicionar a pasta arquivos_mp3
 
     @staticmethod
     def converterArquivo():
         try:
             this.__video_url = YouTube(this.__video_url).streams.get_audio_only()  #
+            this.__video_url.download(rf'{this.__caminho}\arquivos_mp3"')
 
-            this.__video_url.download(rf'{this.__caminho}')
+            title = this.__video_url.title
+
+            subprocess.run([
+                ffmpeg.concat(title, '.mp4')
+                .output(f'{title}.mp3')
+                .run(overwrite_output=True, cmd=rf'{this.__caminho}\ffmpeg\bin\ffmpeg.exe')
+            ])
 
             title = this.__video_url.default_filename
 
@@ -35,5 +45,5 @@ class Converter:
                    f'{this.__caminho}'
 
         except Exception as error:
+            return f'Erro na função {Converter.converterArquivo.__name__}.\n{error}'
             print(f'Erro na função {Converter.converterArquivo.__name__}.\n{error}')
-
--- db_connection.py
+++ db_connection.py
@@ -1,25 +1,27 @@
-import mysql.connector #Importa o MySql
-from mysql.connector import errorcode #Trata as execções que irão surgir
+import mysql.connector  # Importa o MySql
+from mysql.connector import errorcode  # Trata as execções que irão surgir
+
 
 class DbConnection:
 
-def conectar():
-    try:
-        db_connection = mysql.connector.connect(host= 'localhost', user= 'root', password='', database='BancoDeDados')
+    @staticmethod
+    def conectar():
+        try:
+            db_connection = mysql.connector.connect(host= 'localhost', user= 'root', password='', database='BancoDeDados')
 
-        print('Conectado com sucesso!')
-        return db_connection
+            print('Conectado com sucesso!')
+            return db_connection
 
-    except mysql.connector.Error as error: #Guardando as possíveis exceções de mysql.connector na variável 'error'
+        except mysql.connector.Error as error:  # Guardando as possíveis exceções de mysql.connector na variável 'error'
 
-        if error.errno == errorcode.ER_NO_DB_ERROR: #Caso o bando de dados não exista
-            print(f'Banco de dados não existe.\n{error.msg}')
-        elif error.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print(f'Usuário ou senha de acesso ao Banco de Dados estão incorretos.\n{error.msg}')
-        elif error.errno == errorcode.ER_BAD_DB_ERROR:
-            print(f'Erro de conexão com o Banco de dados.\n {error}')
+            if error.errno == errorcode.ER_NO_DB_ERROR:  # Caso o bando de dados não exista
+                print(f'Banco de dados não existe.\n{error.msg}')
+            elif error.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print(f'Usuário ou senha de acesso ao Banco de Dados estão incorretos.\n{error.msg}')
+            elif error.errno == errorcode.ER_BAD_DB_ERROR:
+                print(f'Erro de conexão com o Banco de dados.\n {error}')
+            else:
+                print(error.msg)
+
         else:
-            print(error.msg)
-
-    else:
-        db_connection.close()
+            db_connection.close()
