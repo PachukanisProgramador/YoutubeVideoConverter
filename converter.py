36d981703e83d48b7905e4e9d58e65004a5c6d49
PachukanisProgramador <498YUIPj>
2022-07-11 22:07:34 -0300

    Ultimas mudancas

--- converter.py
+++ converter.py
@@ -30,21 +30,6 @@ class Converter:
         return video_stream
 
     @staticmethod
-    def baixarPlaylist(playlist_url, qualidade):
-        Converter.verificarPasta()
-
-        playlist = Playlist(playlist_url)  # Baixando arquivo MP4 na pior qualidade.
-
-        for video in playlist.videos:
-            if qualidade == 1:
-                video_stream = video.streams.get_lowest_resolution()  # Baixando playlist em MP4 através de list comprehension  na pior qualidade possível.
-                return video_stream
-            elif qualidade == 2:
-                video_stream = video.streams.get_highest_resolution()  # Baixando playlist em MP4 através de list comprehension  na melhor qualidade possível.
-                return video_stream
-
-
-    @staticmethod
     def converterArquivoUnico(video_url, qualidade):  # Função principal onde o arquivo é convertido.
         try:
             # Tratar arquivo e definir caminhos
@@ -70,32 +55,64 @@ class Converter:
             return f'Erro na função {Converter.converterArquivoUnico.__name__}:\n{error}'
 
     @staticmethod
-    def converterArquivosPlaylist(video_url, qualidade):
+    def converterArquivosPlaylist(playlist_url, qualidade):
+
         try:
-            # Tratar arquivo e definir caminhos
-            Converter.baixarPlaylist(video_url, qualidade).download(output_path=rf'{this.__caminho}')  # baixando e informando o caminho em que o arquivo será baixado.
-            arquivo_mp4 = os.path.join(this.__caminho, Converter.baixarPlaylist(video_url, qualidade).default_filename)
-            nome = Converter.baixarPlaylist(video_url, qualidade).default_filename.replace('.mp4', '')
+            Converter.verificarPasta()
 
-            # Especificando o caminho do arquivo que será convertido para mp3
-            caminho_arquivo_convertido = os.path.join(this.__caminho, f'{nome}.mp3')
+            playlist = Playlist(playlist_url)  # Baixando arquivo MP4 na pior qualidade.
 
-            # Convertendo o arquivo para mp3 e salvando no caminho anteriormente instanciado
-            video = VideoFileClip(arquivo_mp4)
-            audio = video.audio
+            for video in playlist.videos:
 
-            audio.write_audiofile(caminho_arquivo_convertido)  # Baixando o áudio no caminho especificado
-            video.close()
-            audio.close()
+                if qualidade == 1:
 
-            this.__dao.inserir(Converter.baixarPlaylist(video_url, qualidade).default_filename, nome)  # Inserindo no banco de dados
+                    nome_arquivo = video.streams.get_lowest_resolution().default_filename
+                    video.streams.get_lowest_resolution().download(output_path=rf'{this.__caminho}')  # Baixando playlist em MP4 na pior qualidade possível.
+                    arquivo_mp4 = os.path.join(this.__caminho, nome_arquivo)
+                    nome = nome_arquivo.replace('.mp4', '')
 
-            os.remove(arquivo_mp4)  # Removendo arquivo mp4 anteriormente baixado
+                    # Especificando o caminho do arquivo que será convertido para mp3
+                    caminho_arquivo_convertido = os.path.join(this.__caminho, f'{nome}.mp3')
+
+                    # Convertendo o arquivo para mp3 e salvando no caminho anteriormente instanciado
+                    video_file = VideoFileClip(arquivo_mp4)
+                    audio = video_file.audio
+
+                    audio.write_audiofile(caminho_arquivo_convertido)  # Baixando o áudio no caminho especificado
+                    video_file.close()
+                    audio.close()
+
+                    this.__dao.inserir(nome_arquivo, nome)  # Inserindo no banco de dados
+
+                    os.remove(arquivo_mp4)  # Removendo arquivo mp4 anteriormente baixado
+
+                elif qualidade == 2:
+
+                    nome_arquivo = video.streams.get_highest_resolution().default_filename
+                    video.streams.get_highest_resolution().download(output_path=rf'{this.__caminho}')  # Baixando playlist em MP4 na pior qualidade possível.
+                    arquivo_mp4 = os.path.join(this.__caminho, nome_arquivo)
+                    nome = nome_arquivo.replace('.mp4', '')
+
+                    # Especificando o caminho do arquivo que será convertido para mp3
+                    caminho_arquivo_convertido = os.path.join(this.__caminho, f'{nome}.mp3')
+
+                    # Convertendo o arquivo para mp3 e salvando no caminho anteriormente instanciado
+                    video_file = VideoFileClip(arquivo_mp4)
+                    audio = video_file.audio
+
+                    audio.write_audiofile(caminho_arquivo_convertido)  # Baixando o áudio no caminho especificado
+                    video_file.close()
+                    audio.close()
+
+                    this.__dao.inserir(nome_arquivo, nome)  # Inserindo no banco de dados
+
+                    os.remove(arquivo_mp4)  # Removendo arquivo mp4 anteriormente baixado
 
             return f'Playlist convertida com sucesso. Os arquivos podem ser encontrados em:\n' \
                    f'{caminho_arquivo_convertido}'
 
         except Exception as error:
+
             return f'Erro na função {Converter.converterArquivosPlaylist.__name__}:\n{error}'
 
     @staticmethod
